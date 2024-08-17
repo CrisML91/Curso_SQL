9448e519ba7c3ec5d0d4dd4b6daf9a65b71dee9e
CrisML91 <cristinamoraleslopez1991@gmail.com>
2024-08-17 10:25:45 +0200

    docs: Se añaden sentencias.

---
+++ Statements/15_alias.sql
@@ -0,0 +1,6 @@
+SELECT name, init_date AS 'Fecha de inicio en programación' FROM users WHERE name = 'Brais';
+
+SELECT name, init_date AS "Fecha de inicio en programación" FROM users WHERE name = "Brais";
+
+SELECT CONCAT(name, ' ', surname) AS 'Nombre completo'FROM users;
+
