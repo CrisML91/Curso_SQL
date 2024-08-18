d29fd6ff9051caf5521909cff21d9abcccdfc4f6
Redosan <redosan1@gmail.com>
2024-08-18 11:53:58 +0200

    docs: Añadidas sentencias.

--- Tables/01_create_table.sql
+++ Tables/01_create_table.sql
@@ -24,7 +24,7 @@ CREATE TABLE persons3(
     UNIQUE(id)
 );
 
-CREATE TABLE persons3(
+CREATE TABLE persons4(
 	id int NOT NULL,
     name varchar(100) NOT NULL,
     age int,
@@ -33,4 +33,39 @@ CREATE TABLE persons3(
     UNIQUE(id),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE persons5(
+	id int NOT NULL,
+    name varchar(100) NOT NULL,
+    age int,
+    email varchar(50),
+    created datetime,
+    UNIQUE(id),
+    PRIMARY KEY(id),
+    CHECK(age>=18)
+);
+
+CREATE TABLE persons6(
+	id int NOT NULL,
+    name varchar(100) NOT NULL,
+    age int,
+    email varchar(50),
+    created datetime DEFAULT CURRENT_TIMESTAMP(),
+    UNIQUE(id),
+    PRIMARY KEY(id),
+    CHECK(age>=18)
+);
+
+CREATE TABLE persons7(
+	id int NOT NULL AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    age int,
+    email varchar(50),
+    created datetime DEFAULT CURRENT_TIMESTAMP(),
+    UNIQUE(id),
+    PRIMARY KEY(id),
+    CHECK(age>=18)
+);
+
+
 
